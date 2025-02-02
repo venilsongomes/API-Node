c841d1121b535f3fe7fdf18cd4817726e78727e9
venilsongomes <49803103+venilsongomes@users.noreply.github.com>
2025-02-02 14:55:51 -0300

    Alteração de obrigatoriedade de inf
    Adicionar  aos  campos nome e age, a opcionalidade, para fim de testes

--- server.js
+++ server.js
@@ -1,18 +1,51 @@
 import express from 'express'
 
-const app = express()
+import { PrismaClient } from '@prisma/client'
 
+const prisma = new PrismaClient()
+
+const app = express()
 app.use(express.json()) // Usar Json
 
-const users  = []
-app.post('/usuarios', (req, res)=>{
-    users.push(req.body)
+app.post('/usuarios', async (req, res)=>{
+    await prisma.user.create({
+        data:{
+            email:req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
     res.status(201).json
 })
-app.get('/usuarios', (req,res)=> {
 
-    res.status(200).json(users)
+app.get('/usuarios',async (req, res) => {
 
-})
+        const users = await prisma.user.findMany()
+          res.status(200).json(users)
+      })
+
+      app.put('/usuarios/:id', async (req, res)=>{
+        await prisma.user.update({
+           where: {
+            id: req.params.id
+           },
+           data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+    
+           }
+        })
+    })
+
+      app.delete('/usuarios/:id', async (req, res)=> {
+
+        await prisma.user.delete({
+            where: {
+                id: req.params.id,
+            },
+        })
+        res.status(200).json({ message:'Usuario deletado com Sucesso!'})
+    })
 
 app.listen(3000)
