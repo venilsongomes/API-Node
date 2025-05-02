63e7114d2a023dd178facdb1c6440eb1658cc9a1
venilsongomes <49803103+venilsongomes@users.noreply.github.com>
2025-05-02 14:04:45 -0300

    Ajustes
    Ajustes

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 import express from 'express'
-
+import cors from 'cors'
 import { PrismaClient } from '@prisma/client'
+
 
 const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
+app.use(cors())
+
+
+app.post('/produtos', async (req, res) => {
+    await prisma.produto.create({
+        data: {
+            name: req.body.name,
+            quantidade: req.body.quantidade
+        }
+    })
+    res.status(201).json({ mensagem: 'Produto criado com sucesso!' })
+})
 
 app.post('/usuarios', async (req, res)=>{
     await prisma.user.create({
@@ -16,6 +29,26 @@ app.post('/usuarios', async (req, res)=>{
         }
     })
     res.status(201).json
+})
+
+
+app.post('/Baixa', async (req, res) => {
+
+    await prisma.Baixa.create({
+        data: {
+            produto: req.body.produto,
+            quantidade: req.body.quantidade,
+            nome: req.body.nome,
+            setor: req.body.setor,
+            data: req.body.data
+ }
+    })
+    res.status(201).json({ mensagem: 'Baixa criada com sucesso!' })
+})
+
+app.get('/produtos', async (req, res) => {
+    const products = await prisma.produto.findMany()
+    res.status(200).json(products)
 })
 
 app.get('/usuarios',async (req, res) => {
@@ -34,6 +67,12 @@ app.get('/usuarios',async (req, res) => {
     
       res.status(200).json(users)
   })
+
+    app.get('/baixa', async (req, res) => { 
+        const baixa = await prisma.baixa.findMany()
+        res.status(200).json(baixa)
+    }
+)
 
 
 /*app.get('/usuarios',async (req, res) => {
@@ -66,5 +105,17 @@ app.delete('/usuarios/:id', async (req, res)=> {
         })
         res.status(200).json({ message:'Usuario deletado com Sucesso!'})
     })
+
+    app.delete('/produtos/:id', async (req, res)=> {
+
+        await prisma.produto.delete({
+            where: {
+                id: req.params.id,
+            },
+        })
+        res.status(200).json({ message:'Produto deletado com Sucesso!'})
+    })
+
+
 
 app.listen(3000)
